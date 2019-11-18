11c1b7219d48cc92c6cf461d000fa87c619278e6
Supradeep T R <tr.supradeep@gmail.com>
2019-11-18 18:38:38 -0500

    Add scale pyramid making of the images
    Added the scale pyramid creation of the images provided.
    
    It uses an averaged bayer pattern to create the scale pyramid.

--- align_merge.py
+++ align_merge.py
@@ -8,6 +8,9 @@ import cv2
 import rawpy
 
 def get_GG_position(raw_pattern):
+    """ input: raw_pattern (np.ndarray) - The raw pattern
+    output: Gr_pos, Gb_pos - Green channel positions"""
+    
     assert(raw_pattern.shape == (2,2))
 
     Gr, Gb  = 1, 3
@@ -22,7 +25,7 @@ def select_ref_frame(raw_imgs, raw_pattern):
         
         This function returns the selected reference frame index 
         after selecting it using a laplacian measure of  sharpness"""
-        
+
     assert(raw_pattern.shape == (2,2))
     assert(raw_imgs.shape[0] >= 3)
 
@@ -46,3 +49,57 @@ def select_ref_frame(raw_imgs, raw_pattern):
     ref_frame_id = np.argmax(sharp)
 
     return ref_frame_id
+
+
+def create_scale_pyramid(raw_imgs):
+    _, h, w = raw_imgs.shape
+    h = h // 2
+    w = w // 2
+
+    img_lvls = []
+
+    # Create the level zero of the scale pyramid: it is average of all
+    # Bayer pattern
+    lvl0 = (raw_imgs[:, 1::2, 0::2] + 
+            raw_imgs[:, 0::2, 0::2] + 
+            raw_imgs[:, 1::2, 1::2] + 
+            raw_imgs[:, 0::2, 1::2]) / 4
+
+    # Pad zeros if it is not a multiple of 32, future downsampling makes sure it is 0
+    lvl0 = np.pad(lvl0,((0, 0),(0, 32 - (h % 32)),(0,32 - (w % 32))), mode='edge')
+
+    img_lvls.append(lvl0)
+
+    # Extract shape of every L0 frame
+    num_tiles, h, w = lvl0.shape
+
+
+    # create gaussian pyramid with L0 -> L1 (down 2x wrt L0) -> L2 (down 8x wrt L0) -> L3 (down 32x wrt L0)
+    lvl1 = np.zeros((num_tiles, h // 2, w // 2))
+    lvl2 = np.zeros((num_tiles, h // 8, w // 8))
+    lvl3 = np.zeros((num_tiles, h // 32, w // 32))
+
+    # Create each level of pyramid 
+    for i in range(num_tiles):
+        frame = lvl0[i,:,:]
+        frame = cv2.pyrDown(frame)
+        lvl1[i,:,:] = frame
+        frame = cv2.pyrDown(frame)
+        frame = cv2.pyrDown(frame)
+        lvl2[i,:,:] = frame
+        frame = cv2.pyrDown(frame)
+        frame = cv2.pyrDown(frame)
+        lvl3[i,:,:] = frame
+
+    # Appending other levels of the pyramids
+    img_lvls.append(lvl1)
+    img_lvls.append(lvl2)
+    img_lvls.append(lvl3)
+
+    return img_lvls
+
+def align_images(ref_img, raw_imgs, use_temp=True):
+
+    img_lvls = create_scale_pyramid(raw_imgs)
+
+    return 0
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_args():
     parser.add_argument('-i', '--input', dest='input', default='./data/33TJ_20150612_201525_012', help='Enter the directory where the files are present')
     parser.add_argument('-o', '--output', dest='out', default='./results/', help='Enter the directory where the output files should be put')
     parser.add_argument('-v', '--verbose', dest='verbose', type=int,default=True, help='If 1, it will inform the steps on the console')
-    parser.add_argument('-t', '--temp', dest='temp', type=int,default=True, help='If 1, it will use the temporary directory "./temp" to load variables, if that is not present, it will create one')
+    parser.add_argument('-t', '--temp', dest='temp', type=int, default=True, help='If 1, it will use the temporary directory "./temp" to load variables, if that is not present, it will create one')
 
     args = parser.parse_args()
     return args
@@ -48,30 +48,7 @@ if __name__ == "__main__":
 
     # average out Bayer BGGR values
     raw_imgs = raw_imgs.astype('double')
-    n,h,w = raw_imgs.shape
-    h = h//2
-    w = w//2
-    lvl0 = (raw_imgs[:,1::2,0::2]+raw_imgs[:,0::2,0::2]+raw_imgs[:,1::2,1::2]+raw_imgs[:,0::2,1::2])/4
-    lvl0 = np.pad(lvl0,((0,0),(0,32-(h%32)),(0,32-(w%32))),mode='edge')
-    n,h,w = lvl0.shape
-    # create gaussian pyramid
-    lvl1 = np.zeros((n,h//2,w//2))
-    lvl2 = np.zeros((n,h//8,w//8))
-    lvl3 = np.zeros((n,h//32,w//32))
-    for i in range(n):
-        frame = lvl0[i,:,:]
-        frame = cv2.pyrDown(frame)
-        lvl1[i,:,:] = frame
-        frame = cv2.pyrDown(frame)
-        frame = cv2.pyrDown(frame)
-        lvl2[i,:,:] = frame
-        frame = cv2.pyrDown(frame)
-        frame = cv2.pyrDown(frame)
-        lvl3[i,:,:] = frame
-    # align level1
-    # align level2
-    # align level3
-    # align level4
+    aligned_raw = align_merge.align_images(raw_imgs[ref_frame_id, ...], raw_imgs)
 
     end_time = time.time()
     print("Total time taken: {}".format(end_time - start_time))
